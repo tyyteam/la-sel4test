739bfb0c877610a2f0f08c6091ac013924230a8b
leoncoci <2461930154@qq.com>
2022-05-02 19:12:24 +0800

    almost_done[231/262]

--- apps/sel4test-driver/src/tests/syscall.c
+++ apps/sel4test-driver/src/tests/syscall.c
@@ -223,6 +223,49 @@
         test_assert(a14 == MAGIC_WORD + 14); \
         test_assert(a15 == MAGIC_WORD + 15); \
     } while (0)
+#elif defined(CONFIG_ARCH_LOONGARCH)
+#define TEST_MRS 0, 0, 0, 0
+#define TEST_REGISTERS(code)                    \
+    do {                                        \
+        register unsigned long a00 = MAGIC_WORD + 0; \
+        register unsigned long a01 = MAGIC_WORD + 1; \
+        register unsigned long a02 = MAGIC_WORD + 2; \
+        register unsigned long a03 = MAGIC_WORD + 3; \
+        register unsigned long a04 = MAGIC_WORD + 4; \
+        register unsigned long a05 = MAGIC_WORD + 5; \
+        register unsigned long a06 = MAGIC_WORD + 6; \
+        register unsigned long a07 = MAGIC_WORD + 7; \
+        register unsigned long a08 = MAGIC_WORD + 8; \
+        register unsigned long a09 = MAGIC_WORD + 9; \
+        register unsigned long a10 = MAGIC_WORD + 10; \
+        register unsigned long a11 = MAGIC_WORD + 11; \
+        register unsigned long a12 = MAGIC_WORD + 12; \
+        register unsigned long a13 = MAGIC_WORD + 13; \
+        register unsigned long a14 = MAGIC_WORD + 14; \
+        register unsigned long a15 = MAGIC_WORD + 15; \
+        code ;                                  \
+        __asm__ __volatile__ (""                \
+                : "+r"(a00), "+r"(a01), "+r"(a02), "+r"(a03), \
+                  "+r"(a04), "+r"(a05), "+r"(a06), "+r"(a07), \
+                  "+r"(a08), "+r"(a10), "+r"(a11), "+r"(a12), \
+                  "+r"(a13));                   \
+        test_assert(a00 == MAGIC_WORD + 0); \
+        test_assert(a01 == MAGIC_WORD + 1); \
+        test_assert(a02 == MAGIC_WORD + 2); \
+        test_assert(a03 == MAGIC_WORD + 3); \
+        test_assert(a04 == MAGIC_WORD + 4); \
+        test_assert(a05 == MAGIC_WORD + 5); \
+        test_assert(a06 == MAGIC_WORD + 6); \
+        test_assert(a07 == MAGIC_WORD + 7); \
+        test_assert(a08 == MAGIC_WORD + 8); \
+        test_assert(a09 == MAGIC_WORD + 9); \
+        test_assert(a10 == MAGIC_WORD + 10); \
+        test_assert(a11 == MAGIC_WORD + 11); \
+        test_assert(a12 == MAGIC_WORD + 12); \
+        test_assert(a13 == MAGIC_WORD + 13); \
+        test_assert(a14 == MAGIC_WORD + 14); \
+        test_assert(a15 == MAGIC_WORD + 15); \
+    } while (0)
 #else
 #error "Unknown architecture."
 #endif
